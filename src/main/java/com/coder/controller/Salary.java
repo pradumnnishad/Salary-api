501fe40c4312abb83fec7db065313ff1ecdebf53
pradumnnishad <pradumnn06@gmail.com>
2025-07-15 11:14:56 +0530

    Comment added

--- src/main/java/com/coder/controller/Salary.java
+++ src/main/java/com/coder/controller/Salary.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Salary {
 
-	
+	// provide salary detail for employee
 	@GetMapping("/salary")
 	public ResponseEntity<Integer> getsalary(){
 		
